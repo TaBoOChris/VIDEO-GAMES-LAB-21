b77fd1d45e27ec556db150818ccffe5a473e3ba1
Florent <florent.bogacz@gmail.com>
2021-11-05 19:14:38 -0400

    Animations

--- VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerController.cs
+++ VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float playerSpeed = 2.0f;
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
+
+
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
@@ -16,13 +18,16 @@ public class PlayerController : MonoBehaviour
     private Vector2 movementInput = Vector2.zero;
     private bool jumped = false;
 
+
+    public Animator animator;
+
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
     }
 
     public void onMove(InputAction.CallbackContext context){
-        movementInput = context.ReadValue<Vector2>();
+        movementInput = new Vector2(context.ReadValue<Vector2>().x , 0).normalized;
     }
 
     public void onJump(InputAction.CallbackContext context){
@@ -42,7 +47,7 @@ public class PlayerController : MonoBehaviour
 
         if (move != Vector3.zero)
         {
-            //gameObject.transform.forward = move;
+            gameObject.transform.forward = new Vector3( 0,0,move.x);
         }
 
         // Changes the height position of the player..
@@ -53,5 +58,8 @@ public class PlayerController : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+
+        animator.SetFloat("Speed", Mathf.Abs( move.x) );
     }
 }
