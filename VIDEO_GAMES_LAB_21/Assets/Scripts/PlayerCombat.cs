2e9c6797e46e007c5c58471fe9b80b99ee693ad7
Florent <florent.bogacz@gmail.com>
2021-11-06 19:46:12 -0400

    Impact with force

--- VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerCombat.cs
+++ VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerCombat.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class PlayerCombat : MonoBehaviour
 {
     public Animator animator;
+
+    public float percentage = 0.0f;
     
     public bool isAttacking = false;
 
@@ -20,11 +22,22 @@ public class PlayerCombat : MonoBehaviour
 
     public void QuickAttack(InputAction.CallbackContext context){
         
+        if(context.performed != true){
+            return;
+        }
+
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,attackRange,stickLayers);
-        Debug.Log(hitEnemies.Length);
      
         foreach ( Collider2D enemy in hitEnemies){
-            Debug.Log("hit " + enemy.name);
+            if(enemy.gameObject != gameObject){
+
+                Debug.Log("hit " + enemy.name);
+
+                float damageImpact = 20.0f;
+
+                enemy.GetComponent<PlayerCombat>().TakeDamage(damageImpact , attackPoint.position);
+               
+            }
         } 
 
         if(context.performed && !isAttacking) { 
@@ -40,6 +53,20 @@ public class PlayerCombat : MonoBehaviour
         if(attackPoint != null){
             Gizmos.DrawWireSphere(attackPoint.position, attackRange);
         }
+    }
+
+    public void TakeDamage(float percent , Vector3 DamageImpactPosition){
+
+
+        Vector2 forceDirection = this.gameObject.transform.position - DamageImpactPosition;
+        forceDirection = forceDirection.normalized;
+        gameObject.GetComponent<Rigidbody2D>().AddForce(forceDirection * percent + forceDirection*percentage);
+
+
+        percentage += percent;
+
+        Debug.Log("current percent : " + percentage);
+        // play hurt anim
     }
 
 }
