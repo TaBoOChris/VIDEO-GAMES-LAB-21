03b31ad8976d5fbd56c4b1dec551c6401deb5de2
Florent <florent.bogacz@gmail.com>
2021-11-06 00:05:13 -0400

    Combo systeme

--- VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerCombat.cs
+++ VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerCombat.cs
@@ -5,24 +5,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     public Animator animator;
-
-    public float attackRate = 2f;
-    float nextAttackTime = 0f;
+    
+    public bool isAttacking = false;
+    
+    private void Awake(){
+        animator = GetComponent<Animator>();
+    }
 
     public void QuickAttack(InputAction.CallbackContext context){
-
-        if(Time.time >= nextAttackTime){
-            // play attack anim
-            animator.SetTrigger("QuickAttack");
-
-            nextAttackTime = Time.time + 1f/attackRate;
-            // deteck enemies
-            // damage them 
-
-        }
+        
+        if(context.performed && !isAttacking) { isAttacking = true; }
     }
 
 }
