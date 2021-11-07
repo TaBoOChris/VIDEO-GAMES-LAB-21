842be84c30436721041fc6ac87ec1b9dd006702f
Florent <florent.bogacz@gmail.com>
2021-11-07 14:20:40 -0500

    fix animation bug air movement

--- VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerMovement.cs
+++ VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public bool isStunned = false;
     private bool jumped = false;
     private bool groundedPlayer;
+    private bool falling = false;
 
     float move = 0.0f;      // get controller axis
     float runSpeed = 40.0f;
@@ -34,8 +35,16 @@ public class PlayerMovement : MonoBehaviour
             controller.Move(move * runSpeed * Time.fixedDeltaTime,false,jumped);
             jumped = false;
 
-
+            // IsFalling
+            if( GetComponent<Rigidbody2D>().velocity.y < -0.01f){
+                falling = true;
+            } 
+                
+            // speed for animation
             animator.SetFloat("Speed", Mathf.Abs( move) );
+
+
+            // Dust Effeck
             if(Mathf.Abs(move)>0){
                 Debug.Log("createDust");
                 createDust();
@@ -46,13 +55,14 @@ public class PlayerMovement : MonoBehaviour
     }   
 
 
-    
+    // receive input for move
     public void onMove(InputAction.CallbackContext context){
 
         move = new Vector2(context.ReadValue<Vector2>().x , 0).normalized.x;
         
     }
 
+    // receive input for jump
     public void onJump(InputAction.CallbackContext context){
 
         if(context.action.triggered){
@@ -64,11 +74,12 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-
+    // when player touch the floor
     public void OnLand(){
-        if(!jumped){
+        if(falling){
 
             animator.SetBool("IsJumping", false);
+            falling = false;
         }
         
     }
