f54df52e256518407bd7cd53c9e55dc25b10d6e1
Florent <florent.bogacz@gmail.com>
2021-11-06 20:24:01 -0400

    Impact with force from animation Event

--- VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerCombat.cs
+++ VIDEO_GAMES_LAB_21/Assets/Scripts/PlayerCombat.cs
@@ -6,14 +6,15 @@ using UnityEngine.InputSystem;
 public class PlayerCombat : MonoBehaviour
 {
     public Animator animator;
-
     public float percentage = 0.0f;
-    
     public bool isAttacking = false;
-
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask stickLayers;
+
+
+
+    //-------------------------------------------------------
     
     private void Awake(){
         animator = gameObject.GetComponent<Animator>();
@@ -25,20 +26,6 @@ public class PlayerCombat : MonoBehaviour
         if(context.performed != true){
             return;
         }
-
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,attackRange,stickLayers);
-     
-        foreach ( Collider2D enemy in hitEnemies){
-            if(enemy.gameObject != gameObject){
-
-                Debug.Log("hit " + enemy.name);
-
-                float damageImpact = 20.0f;
-
-                enemy.GetComponent<PlayerCombat>().TakeDamage(damageImpact , attackPoint.position);
-               
-            }
-        } 
 
         if(context.performed && !isAttacking) { 
                
@@ -55,18 +42,52 @@ public class PlayerCombat : MonoBehaviour
         }
     }
 
-    public void TakeDamage(float percent , Vector3 DamageImpactPosition){
+    public void TakeDamage(float percent){
 
+        percentage += percent;
+        Debug.Log("current percent : " + percentage);
+        // play hurt anim
+    }
+
+    public void Propulse( Vector3 DamageImpactPosition){
 
         Vector2 forceDirection = this.gameObject.transform.position - DamageImpactPosition;
         forceDirection = forceDirection.normalized;
-        gameObject.GetComponent<Rigidbody2D>().AddForce(forceDirection * percent + forceDirection*percentage);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(forceDirection * percentage);
+
+    }
 
 
-        percentage += percent;
 
-        Debug.Log("current percent : " + percentage);
-        // play hurt anim
+
+    public void QuickAttackOne(){
+        QuickAttack(1);
+    }
+    public void QuickAttackTwo(){
+        QuickAttack(2);
+    }
+    public void QuickAttackThree(){
+        QuickAttack(3);
+    }
+
+    public void QuickAttack(int i){
+        Debug.Log("attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,attackRange,stickLayers);
+     
+        foreach ( Collider2D enemy in hitEnemies){
+            if(enemy.gameObject != gameObject){
+
+                Debug.Log("hit " + enemy.name);
+
+                float damageImpact = 20.0f;
+
+                enemy.GetComponent<PlayerCombat>().TakeDamage(damageImpact * i);
+
+                if(i >= 3 )
+                    enemy.GetComponent<PlayerCombat>().Propulse(attackPoint.position);
+               
+            }
+        } 
     }
 
 }
