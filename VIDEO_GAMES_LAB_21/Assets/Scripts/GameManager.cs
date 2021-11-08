2f3e98d006c7d45011e3943d0d62eb93c5c34ab5
Florent <florent.bogacz@gmail.com>
2021-11-07 21:57:52 -0500

    Modify how to end game

--- VIDEO_GAMES_LAB_21/Assets/Scripts/GameManager.cs
+++ VIDEO_GAMES_LAB_21/Assets/Scripts/GameManager.cs
@@ -17,9 +17,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player1.transform.position.y < -10 || player2.transform.position.y < -10 ){
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1); 
-
+        if( player1.transform.position.y < -10 || player1.transform.position.y > 3){
+            player1.transform.position = new Vector2(-10.0f,-10.0f);
+            Invoke("BackMenu", 1.0f);
         }
+
+        if(player2.transform.position.y < -10  || player2.transform.position.y > 3 ){
+            player2.transform.position = new Vector2(-10.0f,-10.0f);
+            Invoke("BackMenu", 1.0f);
+        }
+    }
+
+
+    public void BackMenu(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1); 
+
     }
 }
